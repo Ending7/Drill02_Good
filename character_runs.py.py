e14255fc5db531ab0d4b0dc29784955f29ac7e16
Jin Young Jang - MyPC <ending1025@gmail.com>
2023-09-20 17:43:42 +0900

    전체 뼈대 완성

--- character_runs.py.py
+++ character_runs.py.py
@@ -1,7 +1,27 @@
+import math
+
 from pico2d import *
 
 open_canvas()
 
-# fill here
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+
+def run_circle():
+    print('CIRCLE')
+    pass
+
+def run_rectangle():
+    print('RECTANGLE')
+    pass
+
+
+while True:
+    run_circle()
+    run_rectangle()
+    break;
+
+
 
 close_canvas()
