9895aca67b89be5956129a47218be5640205dd27
Jin Young Jang - MyPC <ending1025@gmail.com>
2023-09-20 18:07:54 +0900

    Draw_Rectangle(bottom)

--- character_runs.py.py
+++ character_runs.py.py
@@ -21,11 +21,16 @@ def run_circle():
 
 def run_rectangle():
     print('RECTANGLE')
-    pass
 
+    #bottom line
+    for x in range(50, 750+1, 10):     
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,90)
+        delay(0.1)
 
 while True:
-    run_circle()
+    #run_circle()
     run_rectangle()
     break;
 
